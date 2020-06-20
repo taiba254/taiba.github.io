2542f7c77a388f95876718eb0535ca6ddc4dd589
taiba254 <67117870+taiba254@users.noreply.github.com>
2020-06-20 18:03:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head><title>Weather Report</title>
+</head>
+<body>
+   
+<center>
+   <form method="GET" action="get.php">
+   <h1>Type City and Country<h1>
+   <br><p><h3>For Example Mumbai</h3></p>
+        <input Type= "text" name="q" required>
+        <input type="submit" name="submit">
+    </form>
+</center>	
+</body>
+</html>
